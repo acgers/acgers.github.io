fd02a043c9b32b332be7f5419562bc9d1b68a68c
wangxufire <wangxufire@gmail.com>
2017-09-03 17:22:15 +0800

    add shelter

--- js/player.js
+++ js/player.js
@@ -13,6 +13,13 @@ var ap = new APlayer({
       url: 'http://ovp1kv2jp.bkt.clouddn.com/mp3secret%20base%20%EF%BD%9E%E5%90%9B%E3%81%8B%E3%82%99%E3%81%8F%E3%82%8C%E3%81%9F%E3%82%82%E3%81%AE%EF%BD%9E%20-%20%E5%B0%8F%E7%BC%98.mp3',
       pic: '../assert/img/secret_base.jpg',
       lrc: '../assert/lrc/secret_base-小缘.lrc'
+    },
+    {
+      title: 'Shelter - Porter Robinson',
+      author: 'Porter Robinson',
+      url: 'http://ovp1kv2jp.bkt.clouddn.com/Shelter-Porter%20Robinson.mp3',
+      pic: '../assert/img/shelter.jpg',
+      lrc: '../assert/lrc/shelter-PorterRobinson.lrc'
     }
   ]
 });
