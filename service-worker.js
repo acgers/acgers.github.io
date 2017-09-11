7d8b24c8a50d6f87da66205ee36c26a5fe564cb6
wangxufire <wangxufire@gmail.com>
2017-09-11 09:16:51 +0800

    use service worker

---
+++ service-worker.js
@@ -0,0 +1,81 @@
+'use strict';
+
+(() => {
+  const cacheVersion = '-sw-v1';
+  const dynamicVendorCacheName = 'dynamic-vendor' + cacheVersion;
+  const staticVendorCacheName = 'static-vendor' + cacheVersion;
+  const staticAssetsCacheName = 'app-assets' + cacheVersion;
+  const jsCacheName = 'app-js' + cacheVersion;
+  const cssCacheName = 'app-css' + cacheVersion;
+  // const contentCacheName = 'content' + cacheVersion;
+  const maxEntries = 50;
+  const maxAgeSeconds = 3600 * 24 * 3;
+
+  self.importScripts('js/sw.js');
+
+  self.toolbox.options.debug = false;
+  self.toolbox.options.networkTimeoutSeconds = 60;
+
+  self.toolbox.router.get('/assets/(.*)', self.toolbox.cacheFirst, {
+    cache: {
+      name: staticAssetsCacheName,
+      maxAgeSeconds: maxAgeSeconds,
+      maxEntries: maxEntries
+    }
+  });
+
+  self.toolbox.router.get('/css', self.toolbox.fastest, {
+    origin: /fonts\.googleapis\.com/,
+    cache: {
+      name: dynamicVendorCacheName,
+      maxAgeSeconds: maxAgeSeconds,
+      maxEntries: maxEntries
+    }
+  });
+
+  self.toolbox.router.get('/(.*)', self.toolbox.cacheFirst, {
+    origin: /(fonts\.gstatic\.com|www\.google-analytics\.com|acgers-1253700126\.cossh\.myqcloud\.com|unpkg\.com)/,
+    cache: {
+      name: staticVendorCacheName,
+      maxAgeSeconds: maxAgeSeconds,
+      maxEntries: maxEntries
+    }
+  });
+
+  self.toolbox.router.get('/js/(.*)', self.toolbox.cacheFirst, {
+    cache: {
+      name: jsCacheName,
+      maxAgeSeconds: maxAgeSeconds,
+      maxEntries: maxEntries
+    }
+  });
+
+  self.toolbox.router.get('/css/(.*)', self.toolbox.cacheFirst, {
+    cache: {
+      name: cssCacheName,
+      maxAgeSeconds: maxAgeSeconds,
+      maxEntries: maxEntries
+    }
+  });
+
+  // self.toolbox.router.get('/*',  (request, values, options) => {
+  //   if (!request.url.match(/(\/ghost\/|\/page\/)/) && request.headers.get('accept').includes('text/html')) {
+  //     return self.toolbox.fastest(request, values, options);
+  //   } else {
+  //     return self.toolbox.networkOnly(request, values, options);
+  //   }
+  // }, {
+  //     cache: {
+  //       name: contentCacheName,
+  //       maxEntries: maxEntries
+  //     }
+  //   });
+
+  self.addEventListener('install', function (event) {
+    return event.waitUntil(self.skipWaiting());
+  });
+
+  self.addEventListener('activate', function (event) {
+    return event.waitUntil(self.clients.claim());
+  });
+})();
