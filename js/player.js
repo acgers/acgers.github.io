b82e40714d95612d2e224c5494049c949ac62f94
wangxufire <wangxufire@gmail.com>
2017-09-11 21:52:00 +0800

    update wrong lrc

--- js/player.js
+++ js/player.js
@@ -26,7 +26,7 @@ var ap = new APlayer({
       author: '小缘',
       url: 'https://acgers-1253700126.cossh.myqcloud.com/secret%20base%20%EF%BD%9E%E5%90%9B%E3%81%8B%E3%82%99%E3%81%8F%E3%82%8C%E3%81%9F%E3%82%82%E3%81%AE%EF%BD%9E%20-%20%E5%B0%8F%E7%BC%98.mp3',
       pic: '../assets/img/secret_base.jpg',
-      lrc: '../assets/lrc/secret_base-小缘.lrc'
+      lrc: '../assets/lrc/secret_base_小缘.lrc'
     },
     {
       title: 'Shelter',
